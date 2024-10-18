12b2740e1ea59f168a1a73d4a42902d7b623d5a8
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-10-18 05:21:04 +0300

    Refactor requirements.txt and add bcrypt and passlib to dependencies

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter , status , Depends
+from .schemas import UserCreateModel, UserModel
+from .service import UserService
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.db.main import get_session
+from typing import Annotated
+from fastapi.exceptions import  HTTPException
 
+user_service = UserService()
 auth_router = APIRouter()
 
-@auth_router.post('/signup')
-async def create_user_account():
-    pass
+@auth_router.post('/signup' , response_model=UserModel, status_code=status.HTTP_201_CREATED)
+async def create_user_account(user_data : UserCreateModel , session : Annotated[AsyncSession, Depends(get_session)] ):
+    email = user_data.email
+    is_exist = await user_service.user_exists_by_email(email , session)
+    if is_exist:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User with email already exists")
+    new_user = await user_service.create_user(user_data, session)
+    return new_user
---
+++ src/auth/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+import uuid
+
+class UserCreateModel(BaseModel):
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
+    username : str = Field(max_length=10)
+    email : str = Field(max_length=50)
+    password : str = Field(min_length=4)
+
+class UserModel(BaseModel):
+    uid : uuid.UUID
+    username : str
+    email : str
+    first_name : str
+    last_name : str
+    is_verified : bool
+    #The original password is never stored
+    password_hash : str = Field(exclude=True)
+    created_at: datetime
+    update_at: datetime
